0349908f5f76d3b89ecd108f22f221d0cb2d33b6
Abdeali Chharchhoda <abdealiking786@gmail.com>
2024-05-27 17:43:39 +0530

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Razorpayx Integration
+
+Automat Payments By RazorPayX API For Frappe Apps
+
+#### License
+
+MIT
---
+++ razorpayx_integration/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Razorpayx Integration",
+			"type": "module",
+			"label": _("Razorpayx Integration")
+		}
+	]
