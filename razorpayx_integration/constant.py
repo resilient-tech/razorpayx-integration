656d3b3164ad5ca09832714eb7bc0c65b77aa1a5
Abdeali Chharchhoda <abdealiking786@gmail.com>
2024-09-05 12:02:49 +0530

    fix: added custom fields and propert setters for Bank Account details

--- razorpayx_integration/constant.py
+++ razorpayx_integration/constant.py
@@ -54,6 +54,7 @@ class RAZORPAYX_PAYOUT_PURPOSE(BaseEnum):
 
 
 class RAZORPAYX_PAYOUT_STATUS(BaseEnum):
+    NOT_INITIATED = "not_initiated"  # custom
     QUEUED = "queued"
     PENDING = "pending"  # if RazorpayX workflow is enabled
     REJECTED = "rejected"  # if RazorpayX workflow is enabled
--- razorpayx_integration/install.py
+++ razorpayx_integration/install.py
@@ -10,37 +10,185 @@ from frappe.permissions import (
 
 from razorpayx_integration.constant import BUG_REPORT_URL, RAZORPAYX_SETTING_DOCTYPE
 
-# todo: some custom fields are remain to add
+INTEGRATION_MANAGER_PERMISSION_LEVEL = 7
+
+
+CUSTOM_BANK_SECTION = {
+    "fieldname": "bank_details_section",
+    "label": "Bank Details",
+    "fieldtype": "Section Break",
+    "permlevel": INTEGRATION_MANAGER_PERMISSION_LEVEL,
+}
+
+CUSTOM_PARTY_FIELDS = [
+    {
+        "fieldname": "bank_account",
+        "label": "Bank Account",
+        "fieldtype": "Link",
+        "options": "Bank Account",
+        "insert_after": "bank_details_section",
+        "permlevel": INTEGRATION_MANAGER_PERMISSION_LEVEL,
+    },
+]
+
 CUSTOM_FIELDS = {
+    "Employee": CUSTOM_PARTY_FIELDS,
+    "Supplier": [
+        {
+            **CUSTOM_BANK_SECTION,
+            "insert_after": "accounts",
+        }
+    ]
+    + CUSTOM_PARTY_FIELDS,
+    "Customer": [
+        {
+            **CUSTOM_BANK_SECTION,
+            "insert_after": "payment_terms",
+        },
+        {
+            "fieldname": "end_bank_section",
+            "fieldtype": "Section Break",
+            "insert_after": "bank_account",
+        },
+    ]
+    + CUSTOM_PARTY_FIELDS,
     "Bank Transaction": [
         {
             "fieldname": "closing_balance",
             "label": "Closing Balance",
             "fieldtype": "Currency",
+            "in_list_view": 1,
             "insert_after": "currency",
+        },
+    ],
+    "Payment Entry": [
+        {
+            "fieldname": "razorpayx_payment_section",
+            "label": "RazorpayX Payment",
+            "fieldtype": "Section Break",
+            "depends_on": "eval:(doc.payment_type=='Pay' && doc.mode_of_payment!='Cash' && doc.paid_from && doc.party)",
+            "insert_after": "contact_email",
+            "permlevel": INTEGRATION_MANAGER_PERMISSION_LEVEL,
+        },
+        {
+            "fieldname": "pay_now",
+            "label": "Make Payment Now",
+            "fieldtype": "Check",
+            "depends_on": "eval:(doc.payment_type=='Pay' && doc.mode_of_payment!='Cash' && doc.paid_from && doc.party)",
+            "insert_after": "razorpayx_payment_section",
+            "print_hide": 1,
+            "permlevel": INTEGRATION_MANAGER_PERMISSION_LEVEL,
+        },
+        {
+            "fieldname": "party_bank_acc",
+            "label": "Party's Bank Account",
+            "fieldtype": "Link",
+            "options": "Bank Account",
+            "depends_on": "eval:doc.pay_now",
+            "mandatory_depends_on": "eval:doc.pay_now",
+            "insert_after": "pay_now",
+            "print_hide": 1,
+            "permlevel": INTEGRATION_MANAGER_PERMISSION_LEVEL,
+        },
+        {
+            "fieldname": "party_bank_ac_no",
+            "label": "Party's Bank Account No",
+            "fieldtype": "Data",
+            "depends_on": "eval:doc.pay_now",
+            "mandatory_depends_on": "eval:doc.pay_now",
+            "insert_after": "party_bank_acc",
+            "read_only": 1,
+            "print_hide": 1,
+            "permlevel": INTEGRATION_MANAGER_PERMISSION_LEVEL,
+        },
+        {
+            "fieldname": "party_bank_branch_code",
+            "label": "Party's Bank Branch Code",
+            "fieldtype": "Data",
+            "depends_on": "eval:doc.pay_now",
+            "mandatory_depends_on": "eval:doc.pay_now",
+            "insert_after": "party_bank_ac_no",
+            "read_only": 1,
+            "print_hide": 1,
+            "permlevel": INTEGRATION_MANAGER_PERMISSION_LEVEL,
+        },
+        {
+            "fieldname": "razorpayx_payment_status",
+            "label": "Payment Status",
+            "fieldtype": "Select",
+            "options": "Not Initiated\nProcessing\nProcessed\nCancelled\nReversed\nFailed",
+            "default": "Not Initiated",
+            "depends_on": "eval:(doc.pay_now && doc.creation)",
+            "insert_after": "party_bank_ac_no",
+            "print_hide": 1,
+            "read_only": 1,
+            "permlevel": INTEGRATION_MANAGER_PERMISSION_LEVEL,
+        },
+        {
+            "fieldname": "bank_payment_cb",
+            "fieldtype": "Column Break",
+            "insert_after": "razorpayx_payment_status",
+        },
+        {
+            "fieldname": "payment_desc",
+            "label": "Payment Description",
+            "fieldtype": "Data",
+            "depends_on": "eval:doc.pay_now",
+            "insert_after": "bank_payment_cb",
+            "print_hide": 1,
+            "reqd": 1,
+            "permlevel": INTEGRATION_MANAGER_PERMISSION_LEVEL,
+        },
+        {
+            "fieldname": "payment_mode",
+            "label": "Payment Mode",
+            "fieldtype": "Select",
+            "options": "\nNEFT\nRTGS",
+            "depends_on": "eval:doc.pay_now",
+            "insert_after": "payment_desc",
+            "reqd": 1,
+            "print_hide": 1,
+            "permlevel": INTEGRATION_MANAGER_PERMISSION_LEVEL,
         },
     ],
 }
 
-# todo: some property setters are remain to add
+
+COMMON_CHECK_PROPERTY_SETTINGS = {
+    "value": 1,
+    "property_type": "Check",
+}
+
+STANDARD_FIELDS_TO_HIDE = {
+    "Bank Transaction": ["transaction_id"],
+    "Employee": ["bank_name", "bank_ac_no", "iban"],
+}
+
 PROPERTY_SETTERS = [
     {
-        "doctype_or_docfield": "DocField",
         "doctype": "Bank Transaction",
         "fieldname": "transaction_id",
         "property": "hidden",
-        "value": 1,
-        "property_type": "Check",
+        **COMMON_CHECK_PROPERTY_SETTINGS,
     },
     {
-        "doctype_or_docfield": "DocField",
         "doctype": "Bank Transaction",
         "fieldname": "transaction_id",
         "property": "print_hide",
-        "value": 1,
-        "property_type": "Check",
+        **COMMON_CHECK_PROPERTY_SETTINGS,
     },
 ]
+
+for doctype, fields in STANDARD_FIELDS_TO_HIDE.items():
+    for field in fields:
+        PROPERTY_SETTERS.append(
+            {
+                "doctype": doctype,
+                "fieldname": field,
+                "property": "hidden",
+                **COMMON_CHECK_PROPERTY_SETTINGS,
+            }
+        )
 
 
 def after_install():
@@ -54,7 +202,7 @@ def after_install():
         click.secho(
             (
                 "Installation for RazorPayX Integration failed due to an error."
-                "Please try re-installing the app or"
+                "Please try re-installing the app or "
                 f"report the issue on {BUG_REPORT_URL} if not resolved."
             ),
             fg="bright_red",
@@ -73,7 +221,6 @@ def make_property_setters():
         frappe.make_property_setter(property_setter)
 
 
-# todo: custom fields and property setters have to set for the role (permissions)
 def make_role_and_permissions():
     role_name = "RazorPayX Integration Manager"
 
@@ -95,5 +242,7 @@ def make_role_and_permissions():
     setup_custom_perms(RAZORPAYX_SETTING_DOCTYPE)
 
     for doctype in ("Employee", "Customer", "Supplier", "Payment Entry"):
-        add_permission(doctype, role_name, 7)
-        update_permission_property(doctype, role_name, 7, "write", 1)
+        add_permission(doctype, role_name, INTEGRATION_MANAGER_PERMISSION_LEVEL)
+        update_permission_property(
+            doctype, role_name, INTEGRATION_MANAGER_PERMISSION_LEVEL, "write", 1
+        )
