b84e16e6f344cf92b7d20715ae5d7c8d9512944d
Abdeali Chharchhoda <abdealiking786@gmail.com>
2024-11-26 12:52:46 +0530

    refactor:  Update `Uninstall` process message

--- razorpayx_integration/uninstall.py
+++ razorpayx_integration/uninstall.py
@@ -1,3 +1,7 @@
+import click
+
+from razorpayx_integration.constants import BUG_REPORT_URL
+from razorpayx_integration.hooks import app_title as APP_NAME
 from razorpayx_integration.setup import (
     delete_custom_fields,
     delete_property_setters,
@@ -6,6 +10,19 @@ from razorpayx_integration.setup import (
 
 
 def before_uninstall():
-    delete_custom_fields()
-    delete_property_setters()
-    delete_role_and_permissions()
+    try:
+        delete_custom_fields()
+        delete_property_setters()
+        delete_role_and_permissions()
+    except Exception as e:
+        click.secho(
+            (
+                f"Uninstallation of {APP_NAME} failed due to an error."
+                "Please try re-uninstalling the app or "
+                f"report the issue on {BUG_REPORT_URL} if not resolved."
+            ),
+            fg="bright_red",
+        )
+        raise e
+
+    click.secho(f"Thank you for using {APP_NAME}!", fg="green")
