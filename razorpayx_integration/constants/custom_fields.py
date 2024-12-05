4249537fc293dd252e4e1ee23b34a40395c47f37
Abdeali Chharchhoda <abdealiking786@gmail.com>
2024-12-05 18:07:11 +0530

    fix: Update custom fields

--- razorpayx_integration/constants/custom_fields.py
+++ razorpayx_integration/constants/custom_fields.py
@@ -1,77 +1,132 @@
-from razorpayx_integration.constants.payouts import RAZORPAYX_PAYOUT_MODE
+"""
+Custom fields which are helpful for payments via RazorpayX
 
-# TODO: add in different parties
-BLOCK_AUTO_PAYMENT = {
-    "fieldname": "block_auto_payment",
-    "label": "Block Auto Payment",
-    "fieldtype": "Check",
-    "description": "Auto payment will be blocked for this party for <strong>RazorpayX</strong>",
-}
+Note:
+    - Keep sequence like this:
+        1. fieldname
+        2. label
+        3. fieldtype
+        4. insert_after
+        ...
+"""
 
+from razorpayx_integration.constants import RAZORPAYX_SETTING_DOCTYPE
+from razorpayx_integration.constants.payouts import (
+    RAZORPAYX_PAYOUT_MODE,
+    RAZORPAYX_PAYOUT_STATUS,
+)
+
+# TODO: permission level are left to add
 
 CUSTOM_FIELDS = {
-    "Bank Transaction": [
-        {
-            "fieldname": "closing_balance",
-            "label": "Closing Balance",
-            "fieldtype": "Currency",
-            "in_list_view": 1,
-            "insert_after": "currency",
-        },
-    ],
     "Bank Account": [
-        # For `Link` payment mode
-        {
-            "fieldname": "party_contact",
-            "label": "Party Contact",
-            "fieldtype": "Data",
-            "insert_after": "party_type",
-            "is_virtual": 1,
-            "depends_on": "eval: !doc.is_company_account && doc.payment_mode === 'Link'",
-            "read_only": 1,
-        },
-        {
-            "fieldname": "party_email",
-            "label": "Party Email",
-            "fieldtype": "Data",
-            "insert_after": "party",
-            "is_virtual": 1,
-            "depends_on": "eval: !doc.is_company_account && doc.payment_mode === 'Link'",
-            "read_only": 1,
-        },
-        # Payment Mode Section
         {
             "fieldname": "payment_section",
             "label": "RazorpayX Payment Details",
             "fieldtype": "Section Break",
-            "insert_after": "party_email",
+            "insert_after": "bank_account_no",
             "depends_on": "eval: !doc.is_company_account",
         },
         {
-            "fieldname": "payment_mode",
-            "label": "Payment Mode",
+            "fieldname": "razorpayx_payment_mode",
+            "label": "RazorpayX Payment Mode",
             "fieldtype": "Select",
             "insert_after": "payment_section",
-            "options": "\n".join(RAZORPAYX_PAYOUT_MODE.values()),
-            "default": RAZORPAYX_PAYOUT_MODE.Bank.value,
+            "options": RAZORPAYX_PAYOUT_MODE.values_as_string(),
+            "default": RAZORPAYX_PAYOUT_MODE.BANK.value,
+        },
+        {
+            "fieldname": "payment_id_cb",
+            "fieldtype": "Column Break",
+            "insert_after": "payment_mode",
         },
         # For `UPI` payment mode
         {
             "fieldname": "upi_id",
             "label": "UPI ID",
             "fieldtype": "Data",
-            "insert_after": "iban",
-            "depends_on": "eval: !doc.is_company_account && doc.payment_mode === 'UPI'",
+            "insert_after": "payment_id_cb",
             "placeholder": "Eg. 90876543@okicici",
+            "depends_on": f"eval: doc.payment_mode === '{RAZORPAYX_PAYOUT_MODE.UPI.value}'",
+        },
+        # For `Link` payment mode
+        {
+            "fieldname": "contact_to_pay",
+            "label": "Contact to Pay",
+            "fieldtype": "Link",
+            "insert_after": "upi_id",
+            "options": "Contact",
+            "depends_on": f"eval: doc.payment_mode === '{RAZORPAYX_PAYOUT_MODE.LINK.value}'",
+            "description": "Contact to whom the payment link will be sent.",
         },
     ],
-    "Customer": [
-        {**BLOCK_AUTO_PAYMENT, "insert_after": "payment_terms"},
-    ],
-    "Supplier": [
-        {**BLOCK_AUTO_PAYMENT, "insert_after": "payment_terms"},
-    ],
-    "Employee": [
-        {**BLOCK_AUTO_PAYMENT, "insert_after": "bank_name"},
+    "Payment Entry": [
+        {
+            "fieldname": "contact_mobile",
+            "label": "Contact Mobile",
+            "fieldtype": "Data",
+            "insert_after": "contact_email",
+            "options": "Phone",
+            "depends_on": "eval: doc.contact_person",
+            "read_only": 1,
+        },
+        {
+            "fieldname": "razorpayx_payment_section",
+            "label": "RazorpayX Payment",
+            "fieldtype": "Section Break",
+            "insert_after": "contact_mobile",
+            "depends_on": "eval: (doc.payment_type=='Pay' && doc.mode_of_payment!='Cash' && doc.paid_from && doc.party)",
+        },
+        {
+            "fieldname": "razorpayx_account",
+            "label": "RazorpayX Integration Account",
+            "fieldtype": "Link",
+            "insert_after": "razorpayx_payment_section",
+            "options": RAZORPAYX_SETTING_DOCTYPE,
+            "print_hide": 1,
+            "read_only": 1,
+            "hidden": 1,
+        },
+        {
+            "fieldname": "razorpayx_payment_mode",
+            "label": "RazorpayX Payment Mode",
+            "fieldtype": "Data",
+            "insert_after": "razorpayx_account",
+            "fetch_from": "party_bank_account.razorpayx_payment_mode",
+            "depends_on": "eval: doc.razorpayx_account && doc.party_bank_account",
+            "mandatory_depends_on": "eval:doc.razorpayx_account && doc.party_bank_account",
+        },
+        {
+            "fieldname": "pay_instantaneous",
+            "label": "Pay Instantaneously",
+            "fieldtype": "Check",
+            "insert_after": "razorpayx_payment_mode",
+            "fetch_from": "party_bank_account.razorpayx_payment_mode",
+            "depends_on": f"eval: doc.razorpayx_account && doc.razorpayx_payment_mode === '{RAZORPAYX_PAYOUT_MODE.BANK.value}'",
+            "description": "Payment will be done with <strong>IMPS</strong> mode.",
+        },
+        {
+            "fieldname": "razorpayx_payment_cb",
+            "fieldtype": "Column Break",
+            "insert_after": "pay_instantaneous",
+        },
+        {
+            "fieldname": "payment_desc",
+            "label": "Payment Description",
+            "fieldtype": "Small Text",
+            "insert_after": "razorpayx_payment_cb",
+            "depends_on": "eval: doc.razorpayx_account",
+            "mandatory_depends_on": "eval: doc.razorpayx_account",
+        },
+        {
+            "fieldname": "razorpayx_payment_status",
+            "label": "Payment Status",
+            "fieldtype": "Select",
+            "insert_after": "payment_desc",
+            "options": RAZORPAYX_PAYOUT_STATUS.values_as_string(),
+            "default": RAZORPAYX_PAYOUT_STATUS.NOT_INITIATED.value,
+            "depends_on": "eval: (doc.razorpayx_account && doc.creation)",
+            "read_only": 1,
+        },
     ],
 }
