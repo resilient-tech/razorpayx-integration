384fb32e615d4473e9086e41f5a9c999ca6d1040
Abdeali Chharchhoda <abdealiking786@gmail.com>
2024-12-11 12:26:02 +0530

    refactor: Update workflow constants to use uppercase naming convention

--- razorpayx_integration/payment_utils/constants/workflows.py
+++ razorpayx_integration/payment_utils/constants/workflows.py
@@ -10,32 +10,32 @@ from razorpayx_integration.payment_utils.constants.enums import BaseEnum
 
 
 class WORKFLOW_ACTIONS(BaseEnum):
-    Approve = "Approve"
-    Hold = "Hold"
-    Reject = "Reject"
-    Review = "Review"
-    Submit = "Submit"
-    Cancel = "Cancel"
+    APPROVE = "Approve"
+    HOLD = "Hold"
+    REJECT = "Reject"
+    REVIEW = "Review"
+    SUBMIT = "Submit"
+    CANCEL = "Cancel"
 
 
 class WORKFLOW_STATES(BaseEnum):
-    Approved = "Approved"
-    Pending = "Pending"
-    Draft = "Draft"
-    Rejected = "Rejected"
-    Submitted = "Submitted"
-    Pending_Approval = "Pending Approval"
-    Cancelled = "Cancelled"
+    APPROVED = "Approved"
+    PENDING = "Pending"
+    DRAFT = "Draft"
+    REJECTED = "Rejected"
+    SUBMITTED = "Submitted"
+    PENDING_APPROVAL = "Pending Approval"
+    CANCELLED = "Cancelled"
 
 
 STATES_COLORS = {
-    WORKFLOW_STATES.Approved.value: "Success",
-    WORKFLOW_STATES.Pending.value: "Warning",
-    WORKFLOW_STATES.Draft.value: "Warning",
-    WORKFLOW_STATES.Rejected.value: "Danger",
-    WORKFLOW_STATES.Submitted.value: "Primary",
-    WORKFLOW_STATES.Pending_Approval.value: "Warning",
-    WORKFLOW_STATES.Cancelled.value: "Danger",
+    WORKFLOW_STATES.APPROVED.value: "Success",
+    WORKFLOW_STATES.PENDING.value: "Warning",
+    WORKFLOW_STATES.DRAFT.value: "Warning",
+    WORKFLOW_STATES.REJECTED.value: "Danger",
+    WORKFLOW_STATES.SUBMITTED.value: "Primary",
+    WORKFLOW_STATES.PENDING_APPROVAL.value: "Warning",
+    WORKFLOW_STATES.CANCELLED.value: "Danger",
 }
 
 
