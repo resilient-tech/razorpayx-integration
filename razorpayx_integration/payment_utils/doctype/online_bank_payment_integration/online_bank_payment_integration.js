d67708df7a914dd51ef2a748574cf326b43a4dbe
Abdeali Chharchhoda <abdealiking786@gmail.com>
2024-12-14 00:22:23 +0530

    revert: delete `Online Bank Payment Integration`

--- razorpayx_integration/payment_utils/doctype/online_bank_payment_integration/online_bank_payment_integration.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, Resilient Tech and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Online Bank Payment Integration", {
-// 	refresh(frm) {
-
-// 	},
-// });
