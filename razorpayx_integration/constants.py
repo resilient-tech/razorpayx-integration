1b118adad27dd42a0aa397dee4ff05c0401940b3
Abdeali Chharchhoda <abdealiking786@gmail.com>
2024-12-13 12:33:11 +0530

    chore: todos

--- razorpayx_integration/constants.py
+++ razorpayx_integration/constants.py
@@ -4,10 +4,13 @@ BUG_REPORT_URL = "https://github.com/resilient-tech/razorpayx_integration/issues
 
 ### Constants for APIs ###
 RAZORPAYX_BASE_API_URL = "https://api.razorpay.com/v1/"
+
+# TODO: instead say SECONDS_IN_A_DAY and then -1 when needed
 SECONDS_IN_A_DAY_MINUS_ONE = 86399  # use for to get day's end epoch time
 
 RAZORPAYX_SUPPORTED_CURRENCY = "INR"
 
+# TODO: make it enum
 RAZORPAYX_SUPPORTED_HTTP_METHODS = (
     "GET",
     "DELETE",
