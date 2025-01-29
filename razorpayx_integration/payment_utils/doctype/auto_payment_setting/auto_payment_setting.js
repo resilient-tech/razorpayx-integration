825d6b90351d24eeda78aa058f44b18ebf9180eb
Smit Vora <smitvora203@gmail.com>
2025-01-29 16:36:13 +0530

    feat: enhance Auto Payment Setting with new filters and options

--- razorpayx_integration/payment_utils/doctype/auto_payment_setting/auto_payment_setting.js
+++ razorpayx_integration/payment_utils/doctype/auto_payment_setting/auto_payment_setting.js
@@ -1,8 +1,15 @@
 // Copyright (c) 2024, Resilient Tech and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Auto Payment Setting", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Auto Payment Setting", {
+	setup: function (frm) {
+		frm.set_query("bank_account", function () {
+			return {
+				filters: {
+					is_company_account: 1,
+					disabled: 0,
+				},
+			};
+		});
+	},
+});
