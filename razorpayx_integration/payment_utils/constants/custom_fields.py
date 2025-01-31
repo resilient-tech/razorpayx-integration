ac1971158cedf056359519afb00f9b92b86ad171
Smit Vora <smitvora203@gmail.com>
2025-01-31 15:22:04 +0530

    feat: auto create and submit payment entries

--- razorpayx_integration/payment_utils/constants/custom_fields.py
+++ razorpayx_integration/payment_utils/constants/custom_fields.py
@@ -132,4 +132,39 @@ CUSTOM_FIELDS = {
             "permlevel": PERMISSION_LEVEL.SEVEN.value,
         },
     ],
+    "Supplier": [
+        {
+            "fieldname": "auto_payment_section",
+            "label": "Auto Payment Settings",
+            "fieldtype": "Section Break",
+            "insert_after": "release_date",
+        },
+        {
+            "fieldname": "disable_auto_generate_payment_entry",
+            "label": "Disable Auto Generate Payment Entry",
+            "fieldtype": "Check",
+            "insert_after": "auto_payment_section",
+            "permlevel": PERMISSION_LEVEL.SEVEN.value,
+        },
+        {
+            "fieldname": "auto_payment_cb",
+            "fieldtype": "Column Break",
+            "insert_after": "disable_auto_generate_payment_entry",
+        },
+        {
+            "fieldname": "disable_auto_submit_entries",
+            "label": "Disable Auto Submit Entries",
+            "fieldtype": "Check",
+            "insert_after": "auto_payment_cb",
+            "permlevel": PERMISSION_LEVEL.SEVEN.value,
+        },
+        {
+            "fieldname": "payment_threshold",
+            "label": "Payment Threshold",
+            "fieldtype": "Currency",
+            "insert_after": "disable_auto_submit_entries",
+            "permlevel": PERMISSION_LEVEL.SEVEN.value,
+            "description": "Overriding the default payment threshold to submit payment entries. Set zero to use default threshold.",
+        },
+    ],
 }
