6cb312abb6d7f15277d194be787d7a6ef8ae6cfb
Abdeali Chharchhoda <abdealiking786@gmail.com>
2025-01-31 19:01:50 +0530

    refactor: Simplify RazorpayX settings retrieval and remove unused function

--- razorpayx_integration/razorpayx_integration/utils/__init__.py
+++ razorpayx_integration/razorpayx_integration/utils/__init__.py
@@ -1,54 +1,7 @@
-from typing import Literal
-
 import frappe
 from erpnext.accounts.doctype.payment_entry.payment_entry import PaymentEntry
-from frappe import _
 
 from razorpayx_integration.constants import RAZORPAYX_SETTING
-
-
-# TODO: remove this and direct  use in webhook
-@frappe.request_cache
-def get_razorpayx_setting(
-    identifier: str,
-    search_by: Literal["bank_account", "account_id"],
-    fields: list[str] | None = None,
-    as_dict: bool = True,
-) -> dict:
-    """
-    Fetch the RazorpayX Integration Setting name based on the identifier.
-
-    :param identifier: The identifier to search by.
-    :param search_by: Field to search by.
-    :param fields: Fields to fetch.
-    :param as_dict: Return as dict or not.
-
-    ---
-    Note:
-    - `bank_account` is company's bank account.
-    """
-    if search_by == "account_id" and identifier.startswith("acc_"):
-        identifier = identifier.removeprefix("acc_")
-
-    return (
-        frappe.db.get_value(
-            doctype=RAZORPAYX_SETTING,
-            filters={
-                search_by: identifier,
-            },
-            fieldname=fields or "name",
-            as_dict=as_dict,
-        )
-        or frappe._dict()
-    )
-
-
-def get_enabled_razorpayx_settings() -> list[str]:
-    return frappe.get_all(
-        RAZORPAYX_SETTING,
-        filters={"disabled": 0},
-        pluck="name",
-    )
 
 
 def is_already_paid(amended_from: str | None = None) -> bool | int:
