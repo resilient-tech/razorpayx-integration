52c81943970e3b01e860e9479b5891e31f9a245d
Abdeali Chharchhoda <abdealiking786@gmail.com>
2025-02-01 11:54:04 +0530

    fix: streamline payout checks in onload function

--- razorpayx_integration/razorpayx_integration/server_overrides/payment_entry.py
+++ razorpayx_integration/razorpayx_integration/server_overrides/payment_entry.py
@@ -36,14 +36,11 @@ UTR_PLACEHOLDER = "*** UTR WILL BE SET AUTOMATICALLY ***"
 def onload(doc: PaymentEntry, method=None):
     doc.set_onload("is_already_paid", is_already_paid(doc.amended_from))
 
-    if not is_payout_via_razorpayx(doc):
-        return
-
     doc.set_onload(
         "has_payout_permission", has_payout_permissions(doc.name, throw=False)
     )
 
-    if doc.docstatus == 1:
+    if doc.docstatus == 1 and is_payout_via_razorpayx(doc):
         doc.set_onload(
             "auto_cancel_payout_enabled",
             is_auto_cancel_payout_enabled(doc.integration_docname),
