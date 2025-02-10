05f054934ba442a14911fd50c5a89b7836c52214
Abdeali Chharchhoda <abdealiking786@gmail.com>
2025-02-10 17:12:45 +0530

    fix: Enhance bank payment mode validation and limits in Payment Entry

--- razorpayx_integration/payment_utils/client_overrides/payment_entry.js
+++ razorpayx_integration/payment_utils/client_overrides/payment_entry.js
@@ -1,3 +1,5 @@
+const PAYMENT_MODE = payment_utils.BANK_PAYMENT_MODE;
+
 frappe.ui.form.on("Payment Entry", {
 	refresh: function (frm) {
 		// update descriptions
@@ -6,9 +8,17 @@ frappe.ui.form.on("Payment Entry", {
 	},
 
 	validate: function (frm) {
+		if (!frm.doc.integration_doctype || !frm.doc.integration_docname) {
+			if (frm.doc.make_bank_online_payment) {
+				frm.set_value("make_bank_online_payment", 0);
+			}
+
+			return;
+		}
+
 		if (!frm.doc.make_bank_online_payment) return;
 
-		if (frm.doc.bank_payment_mode === payment_utils.BANK_PAYMENT_MODE.LINK) {
+		if (frm.doc.bank_payment_mode === PAYMENT_MODE.LINK) {
 			if (!frm.doc.contact_mobile && !frm.doc.contact_email) {
 				let msg = "";
 
@@ -22,7 +32,35 @@ frappe.ui.form.on("Payment Entry", {
 			}
 		}
 
-		// TODO: CHECK FOR MODE LIMIT AND MINIMUM AMOUNT
+		if (frm.doc.bank_payment_mode === PAYMENT_MODE.IMPS && frm.doc.paid_amount > 5_00_000) {
+			frappe.throw({
+				message: __("Amount should be less than {0} for <strong>{1}</strong> transfer", [
+					format_currency(5_00_000, "INR"),
+					PAYMENT_MODE.IMPS,
+				]),
+				title: __("Amount Limit Exceeded"),
+			});
+		}
+
+		if (frm.doc.bank_payment_mode === PAYMENT_MODE.UPI && frm.doc.paid_amount > 1_00_000) {
+			frappe.throw({
+				message: __("Amount should be less than {0} for <strong>{1}</strong> transfer", [
+					format_currency(1_00_000, "INR"),
+					PAYMENT_MODE.UPI,
+				]),
+				title: __("Amount Limit Exceeded"),
+			});
+		}
+
+		if (frm.doc.bank_payment_mode === PAYMENT_MODE.RTGS && frm.doc.paid_amount < 2_00_000) {
+			frappe.throw({
+				message: __("Amount should be greater than {0} for <strong>{1}</strong> transfer", [
+					format_currency(2_00_000, "INR"),
+					PAYMENT_MODE.RTGS,
+				]),
+				title: __("Insufficient Amount"),
+			});
+		}
 	},
 
 	bank_account: async function (frm) {
