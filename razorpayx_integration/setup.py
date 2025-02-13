574b06236f6ab7b7aa5ee6bdaa8f93ff945e8392
Abdeali Chharchhoda <abdealiking786@gmail.com>
2025-02-13 16:34:44 +0530

    refactor: Remove unused workflows and payment utils setup functions

--- razorpayx_integration/setup.py
+++ razorpayx_integration/setup.py
@@ -1,76 +1,42 @@
-"""
-⚠️ Notes:
-- Always first customize the `Payment Utils` and then other Integrations.
-- Always first delete the other Integrations and then `Payment Utils`.
-
-"""
-
 import click
+import frappe
+from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
+from payment_integration_utils.payment_integration_utils.utils import (
+    delete_custom_fields,
+    delete_property_setters,
+    delete_roles_and_permissions,
+    make_roles_and_permissions,
+)
 
-from razorpayx_integration.hooks import app_title as APP_NAME
-from razorpayx_integration.payment_utils.setup import *
-from razorpayx_integration.razorpayx_integration.setup import *
+from razorpayx_integration.razorpayx_integration.constants.custom_fields import (
+    CUSTOM_FIELDS,
+)
+from razorpayx_integration.razorpayx_integration.constants.property_setters import (
+    PROPERTY_SETTERS,
+)
+from razorpayx_integration.razorpayx_integration.constants.roles import ROLES
 
 
-##### After Install Setup #####
+################### After Install ###################
 def setup_customizations():
-    make_roles_and_permissions()
-    make_custom_fields()
-    make_property_setters()
-    make_workflows()
-
-
-def make_roles_and_permissions():
     click.secho("Creating Roles and Permissions...", fg="blue")
+    make_roles_and_permissions(ROLES)
 
-    make_payment_utils_roles_and_permissions()
-    make_razorpayx_roles_and_permissions()
-
-
-def make_custom_fields():
     click.secho("Creating Custom Fields...", fg="blue")
+    create_custom_fields(CUSTOM_FIELDS)
 
-    make_payment_utils_custom_fields()
-    make_razorpayx_custom_fields()
-
-
-def make_property_setters():
     click.secho("Creating Property Setters...", fg="blue")
-
-    make_payment_utils_property_setters()
-    make_razorpayx_property_setters()
-
-
-def make_workflows():
-    click.secho("Creating Workflows...", fg="blue")
-
-    make_payment_utils_workflows()
-    make_razorpayx_workflows()
+    for property_setter in PROPERTY_SETTERS:
+        frappe.make_property_setter(property_setter)
 
 
-##### Before Uninstall Setup #####
+################### Before Uninstall ###################
 def delete_customizations():
-    delete_property_setters()
-    delete_custom_fields()
-    delete_roles_and_permissions()
-
-
-def delete_custom_fields():
     click.secho("Deleting Custom Fields...", fg="blue")
+    delete_custom_fields(CUSTOM_FIELDS)
 
-    delete_razorpayx_custom_fields()
-    delete_payment_utils_custom_fields()
-
-
-def delete_property_setters():
     click.secho("Deleting Property Setters...", fg="blue")
+    delete_property_setters(PROPERTY_SETTERS)
 
-    delete_razorpayx_property_setters()
-    delete_payment_utils_property_setters()
-
-
-def delete_roles_and_permissions():
     click.secho("Deleting Roles and Permissions...", fg="blue")
-
-    delete_razorpayx_role_and_permissions()
-    delete_payment_utils_role_and_permissions()
+    delete_roles_and_permissions(ROLES)
