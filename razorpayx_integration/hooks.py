adb267de8d091ca7e6da6bc6414d7067390b2b32
Abdeali Chharchhoda <abdealiking786@gmail.com>
2025-02-17 17:31:03 +0530

    refactor: update hooks

--- razorpayx_integration/hooks.py
+++ razorpayx_integration/hooks.py
@@ -4,7 +4,7 @@ app_publisher = "Resilient Tech"
 app_description = "Automat Payments By RazorpayX API For Frappe Apps"
 app_email = "info@resilient.tech"
 app_license = "GNU General Public License (v3)"
-required_apps = ["frappe/erpnext/payment_integration_utils"]
+required_apps = ["frappe/erpnext"]
 
 after_install = "razorpayx_integration.install.after_install"
 before_uninstall = "razorpayx_integration.uninstall.before_uninstall"
@@ -14,18 +14,35 @@ app_include_js = "razorpayx_integration.bundle.js"
 export_python_type_annotations = True
 
 doctype_js = {
-    "Payment Entry": "razorpayx_integration/client_overrides/form/payment_entry.js",
+    "Payment Entry": [
+        "integration_utils/client_overrides/form/payment_entry.js",
+        "razorpayx_integration/client_overrides/form/payment_entry.js",
+    ],
     "Bank Reconciliation Tool": "razorpayx_integration/client_overrides/form/bank_reconciliation_tool.js",
+    "Bank Account": "integration_utils/client_overrides/form/bank_account.js",
+    "User": "integration_utils/client_overrides/form/user.js",
 }
 
+doctype_list_js = {
+    "Payment Entry": "integration_utils/client_overrides/list/payment_entry_list.js",
+}
 
 doc_events = {
     "Payment Entry": {
-        "onload": "razorpayx_integration.razorpayx_integration.server_overrides.payment_entry.onload",
-        "validate": "razorpayx_integration.razorpayx_integration.server_overrides.payment_entry.validate",
+        "onload": [
+            "razorpayx_integration.integration_utils.server_overrides.doctype.payment_entry.onload",
+            "razorpayx_integration.razorpayx_integration.server_overrides.payment_entry.onload",
+        ],
+        "validate": [
+            "razorpayx_integration.integration_utils.server_overrides.doctype.payment_entry.validate",
+            "razorpayx_integration.razorpayx_integration.server_overrides.payment_entry.validate",
+        ],
         "before_submit": "razorpayx_integration.razorpayx_integration.server_overrides.payment_entry.before_submit",
         "on_submit": "razorpayx_integration.razorpayx_integration.server_overrides.payment_entry.on_submit",
         "before_cancel": "razorpayx_integration.razorpayx_integration.server_overrides.payment_entry.before_cancel",
+    },
+    "Bank Account": {
+        "validate": "razorpayx_integration.integration_utils.server_overrides.doctype.bank_account.validate",
     },
 }
 
@@ -41,3 +58,7 @@ payment_integration_fields = [
     "razorpayx_payout_id",
     "razorpayx_payout_link_id",
 ]
+
+before_payment_authentication = (
+    "razorpayx_integration.integration_utils.utils.permission.has_payment_permissions"
+)
