4e67596166392f156d8dbe55227ba9fd7159f86b
Abdeali Chharchhoda <abdealiking786@gmail.com>
2025-03-13 14:10:03 +0530

    fix: Working code for payout report

--- razorpayx_integration/razorpayx_integration/report/razorpayx_payout_status/razorpayx_payout_status.py
+++ razorpayx_integration/razorpayx_integration/report/razorpayx_payout_status/razorpayx_payout_status.py
@@ -3,6 +3,10 @@
 
 import frappe
 from frappe import _
+from frappe.query_builder.functions import Date
+from frappe.utils.data import get_timespan_date_range
+
+from razorpayx_integration.constants import RAZORPAYX_CONFIG
 
 
 def execute(filters=None):
@@ -10,11 +14,79 @@ def execute(filters=None):
 
 
 def get_data(filters: dict | None = None) -> list[dict]:
-    pass
+    from_date, to_date = parse_date_range(filters)
+
+    PE = frappe.qb.DocType("Payment Entry")
+
+    base_query = (
+        frappe.qb.from_(PE)
+        .select(
+            PE.name.as_("payment_entry"),
+            PE.company,
+            PE.posting_date,
+            PE.integration_docname.as_("razorpayx_config"),
+            PE.status.as_("docstatus"),
+            PE.payment_transfer_method.as_("payout_mode"),
+            PE.razorpayx_payout_id.as_("payout_id"),
+            PE.razorpayx_payout_link_id.as_("payout_link_id"),
+            PE.razorpayx_payout_desc.as_("payout_description"),
+            PE.razorpayx_payout_status.as_("payout_status"),
+            PE.paid_amount,
+            PE.reference_no.as_("utr"),
+            PE.payment_authorized_by.as_("payout_made_by"),
+        )
+        .where(PE.company == filters.company)
+        .where(PE.posting_date >= Date(from_date))
+        .where(PE.posting_date <= Date(to_date))
+        .where(PE.integration_doctype == RAZORPAYX_CONFIG)
+        .where(PE.make_bank_online_payment == 1)
+    )
+
+    # update the query based on filters
+    if filters.docstatus:
+        base_query = base_query.where(PE.status.isin(filters.docstatus))
+
+    if filters.payout_status:
+        base_query = base_query.where(
+            PE.razorpayx_payout_status.isin(filters.payout_status)
+        )
+
+    if filters.payout_mode:
+        base_query = base_query.where(
+            PE.payment_transfer_method.isin(filters.payout_mode)
+        )
+
+    if filters.razorpayx_config:
+        base_query = base_query.where(
+            PE.integration_docname == filters.razorpayx_config
+        )
+
+    if filters.payout_made_by:
+        base_query = base_query.where(
+            PE.payment_authorized_by == filters.payout_made_by
+        )
+
+    if filters.ignore_amended:
+        base_query = base_query.where(PE.amended_from.isnull())
+
+    return base_query.run(as_dict=True)
+
+
+def parse_date_range(filters: dict) -> tuple[str, str]:
+    if filters.date_time_span == "Select Date Range":
+        return filters.date_range
+
+    return get_timespan_date_range(filters.date_time_span.lower())
 
 
 def get_columns() -> list[dict]:
     return [
+        {
+            "label": _("Payment Entry"),
+            "fieldname": "payment_entry",
+            "fieldtype": "Link",
+            "options": "Payment Entry",
+        },
         {
             "label": _("Company"),
             "fieldname": "company",
@@ -50,6 +122,11 @@ def get_columns() -> list[dict]:
         {
             "label": _("Payout Link ID"),
             "fieldname": "payout_link_id",
+            "fieldtype": "Data",
+        },
+        {
+            "label": _("Payout Description"),
+            "fieldname": "payout_description",
             "fieldtype": "Data",
         },
         {
