2c08fc93786897f3eae2bf3400047cf246211253
Abdeali Chharchhodawala <99460106+Abdeali099@users.noreply.github.com>
2025-03-18 16:05:06 +0530

    feat: report for RazorpayX Payout Status (#72)
    * feat: report for RazorpayX Payout Status
    
    * fix: Add filters
    
    * refactor: Add Payout Status constants for RazorpayX integration
    
    * fix: Proper filters for report
    
    * fix: Minor changes in filters
    
    * fix: define columns for report
    
    * fix: Add 'Ignore Amended' checkbox to RazorpayX Payout Status report
    
    * fix: Remove typo in doc status filter
    
    * fix: Working code for payout report
    
    * fix: Rename report
    
    * refactor: reorder columns
    
    * refactor: use global constant for payout status
    
    * refactor: formatted report value
    
    * refactor: Update the width of column
    
    * docs: Update  doc for report
    
    * refactor: remove `Total Row` bug error
    
    * refactor: Rename report
    
    * docs: Update new report name
    
    * fix: update payout status colors for better visibility
    
    * revert: remove 'Ignore Amended' option from payout status report
    
    * fix: add 'Today' and 'Yesterday' options to timespan filter
    
    * fix: correct filter name from 'time_span' to 'date_time_span' in payout status report
    
    * fix: add 'Party Type' and 'Party' fields to report
    
    * fix: Update column sequence
    
    * fix: add 'Party Type' and 'Party' filters to payout status report query
    
    * fix: reorder 'Posting Date' column in payout status report
    
    * fix: update payout status report image link

---
+++ docs/report/1_payout_status_report.md
@@ -0,0 +1,24 @@
+# 📊 RazorpayX Payout Status Report
+
+This report provides a quick overview of:  
+
+- **Payout Status**  
+- **UTR Details**  
+- **Payout Mode**  etc...
+
+## 🔍 Filters
+
+**You can filter the report by:**
+
+- **Posting Date**: Filter payouts by specific dates.  
+- **Payout Status**: Filter by status (e.g., Processed, Failed, Queued).  
+- **Doc Status**: Filter by document status (e.g., Submitted, Canceled).  
+- **Payout Mode**: Filter by payment method (e.g., NEFT, UPI, Link).  
+
+## 🔎 Accessing the Report
+
+- In the **Search Bar (Awesome Bar)**, type `RazorpayX Payout Status` to view the report.  
+
+**Example**:
+
+![Payout Status Report](https://github.com/user-attachments/assets/3885bc79-771e-42a8-8ddf-aff6d2937dd7)
---
+++ razorpayx_integration/razorpayx_integration/report/razorpayx_payout_status/razorpayx_payout_status.js
@@ -0,0 +1,162 @@
+// Copyright (c) 2025, Resilient Tech and contributors
+// For license information, please see license.txt
+
+const DOC_STATUS = { Draft: "grey", Submitted: "blue", Cancelled: "red" };
+
+const TIMESPANS = [
+	"Today",
+	"Yesterday",
+	"This Week",
+	"This Month",
+	"This Quarter",
+	"This Year",
+	"Last Week",
+	"Last Month",
+	"Last Quarter",
+	"Last Year",
+	"Select Date Range",
+];
+
+const UTR_PLACEHOLDER = "*** UTR WILL BE SET AUTOMATICALLY ***";
+
+frappe.query_reports["RazorpayX Payout Status"] = {
+	filters: [
+		{
+			fieldname: "company",
+			label: __("Company"),
+			fieldtype: "Link",
+			options: "Company",
+			default: frappe.defaults.get_user_default("Company"),
+			reqd: 1,
+		},
+		{
+			fieldname: "date_time_span",
+			label: __("Posting Date"),
+			fieldtype: "Select",
+			options: TIMESPANS,
+			default: "This Month",
+			reqd: 1,
+			on_change: (report) => {
+				if (report.get_filter_value("date_time_span") === "Select Date Range") {
+					const date_range = report.get_filter("date_range");
+					date_range.df.reqd = 1;
+					date_range.set_required(1);
+				}
+
+				report.refresh();
+			},
+		},
+		{
+			fieldname: "date_range",
+			fieldtype: "DateRange",
+			label: __("Posting Date Range"),
+			depends_on: "eval: doc.date_time_span === 'Select Date Range'",
+			default: [frappe.datetime.month_start(), frappe.datetime.now_date()],
+		},
+		{
+			fieldname: "party_type",
+			label: __("Party Type"),
+			fieldtype: "Link",
+			options: "Party Type",
+			get_query: function () {
+				return {
+					filters: {
+						name: ["in", Object.keys(frappe.boot.party_account_types)],
+					},
+				};
+			},
+		},
+		{
+			fieldname: "party",
+			label: __("Party"),
+			fieldtype: "Dynamic Link",
+			options: "party_type",
+		},
+		{
+			fieldname: "docstatus",
+			label: __("Document Status"),
+			fieldtype: "MultiSelectList",
+			get_data: () => get_multiselect_options(Object.keys(DOC_STATUS)),
+		},
+		{
+			fieldname: "payout_status",
+			label: __("Payout Status"),
+			fieldtype: "MultiSelectList",
+			get_data: () => get_multiselect_options(Object.keys(razorpayx.PAYOUT_STATUS)),
+		},
+		{
+			fieldname: "payout_mode",
+			label: __("Payout Mode"),
+			fieldtype: "MultiSelectList",
+			get_data: () =>
+				get_multiselect_options(Object.values(payment_integration_utils.PAYMENT_TRANSFER_METHOD)),
+		},
+		{
+			fieldname: "razorpayx_config",
+			label: __("RazorpayX Configuration"),
+			fieldtype: "Link",
+			options: "RazorpayX Configuration",
+			get_query: function () {
+				return {
+					filters: { company: frappe.query_report.get_filter_value("company") },
+				};
+			},
+		},
+		{
+			fieldname: "payout_made_by",
+			label: __("Payout Made By"),
+			fieldtype: "Link",
+			options: "User",
+		},
+	],
+
+	onload: function (report) {
+		const docstatus = report.get_filter("docstatus");
+
+		if (docstatus && (!docstatus.get_value() || docstatus.get_value().length === 0)) {
+			docstatus.set_value("Submitted");
+		}
+	},
+
+	formatter: function (value, row, column, data, default_formatter) {
+		value = default_formatter(value, row, column, data);
+
+		if (column.fieldname === "docstatus") {
+			value = this.get_formatted_docstatus(value);
+		} else if (column.fieldname === "payout_status") {
+			value = this.get_formatted_payout_status(value);
+		} else if (column.fieldname === "utr") {
+			if (value === UTR_PLACEHOLDER) {
+				value = `<span class="text-muted">-</span>`;
+			}
+		}
+
+		return value;
+	},
+
+	get_formatted_docstatus: function (value) {
+		return `<div class="text-center">
+					<span class="indicator-pill ${DOC_STATUS[value]} no-indicator-dot ellipsis">
+						<span class="ellipsis">${value}</span>
+					</span>
+				</div>`;
+	},
+
+	get_formatted_payout_status: function (value) {
+		return `<div class="indicator ${razorpayx.PAYOUT_STATUS[value]}">
+					<strong>${value}</strong>
+				</div>`;
+	},
+};
+
+function get_multiselect_options(values) {
+	const options = [];
+	for (const option of values) {
+		options.push({
+			value: option,
+			label: option,
+			description: "",
+		});
+	}
+	return options;
+}
