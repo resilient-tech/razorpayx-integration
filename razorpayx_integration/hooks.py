de5c06f3e53565a05c178f56f9b77bcb6bac04e8
Abdeali Chharchhoda <abdealiking786@gmail.com>
2025-03-25 17:33:35 +0530

    feat: add before_tests hook

--- razorpayx_integration/hooks.py
+++ razorpayx_integration/hooks.py
@@ -12,6 +12,8 @@ before_uninstall = "razorpayx_integration.uninstall.before_uninstall"
 after_app_install = "razorpayx_integration.install.after_app_install"
 before_app_uninstall = "razorpayx_integration.uninstall.before_app_uninstall"
 
+before_tests = "razorpayx_integration.tests.before_tests"
+
 app_include_js = "razorpayx_integration.bundle.js"
 
 export_python_type_annotations = True
