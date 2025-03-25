b0dcaa981c73ed9dd62351b582b5e51858ded835
Abdeali Chharchhoda <abdealiking786@gmail.com>
2025-03-25 17:01:46 +0530

    fix: update install script to use PR_BRANCH for razorpayx integration

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -55,7 +55,7 @@ sed -i 's/redis_socketio:/# redis_socketio:/g' Procfile
 
 bench get-app erpnext --branch "$BASE_BRANCH" --resolve-deps
 bench get-app https://github.com/resilient-tech/payment_integration_utils.git --branch "$BASE_BRANCH"
-bench get-app https://github.com/resilient-tech/razorpayx-integration.git --branch "${github.head_ref}"
+bench get-app https://github.com/resilient-tech/razorpayx-integration.git --branch "$PR_BRANCH"
 bench setup requirements --dev
 
 wait $wkpid
