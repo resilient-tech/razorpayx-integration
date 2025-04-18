83cc32bb051bfca54a02fb8d81c61b3840641a82
Abdeali Chharchhoda <abdealiking786@gmail.com>
2025-04-18 12:10:02 +0530

    chore: covert indentation to spaces
    (cherry picked from commit 2a0bfb14e15b112db56348a4d5b063f00a431d96)

--- razorpayx_integration/razorpayx_integration/client_overrides/form/bank_reconciliation_tool.js
+++ razorpayx_integration/razorpayx_integration/client_overrides/form/bank_reconciliation_tool.js
@@ -3,46 +3,46 @@
 const SYNC_BTN_LABEL = __("Sync via RazorpayX");
 
 frappe.ui.form.on("Bank Reconciliation Tool", {
-	refresh: async function (frm) {
-		frm.add_custom_button(SYNC_BTN_LABEL, async () => {
-			await sync_transactions_with_razorpayx(frm.doc.bank_account, frm.__razorpayx_config);
+    refresh: async function (frm) {
+        frm.add_custom_button(SYNC_BTN_LABEL, async () => {
+            await sync_transactions_with_razorpayx(frm.doc.bank_account, frm.__razorpayx_config);
 
-			frappe.show_alert({
-				message: __("<strong>{0}</strong> transactions synced successfully!", [frm.doc.bank_account]),
-				indicator: "green",
-			});
-		});
+            frappe.show_alert({
+                message: __("<strong>{0}</strong> transactions synced successfully!", [frm.doc.bank_account]),
+                indicator: "green",
+            });
+        });
 
-		await toggle_sync_btn(frm);
-	},
+        await toggle_sync_btn(frm);
+    },
 
-	bank_account: async function (frm) {
-		await toggle_sync_btn(frm);
-	},
+    bank_account: async function (frm) {
+        await toggle_sync_btn(frm);
+    },
 });
 
 function sync_transactions_with_razorpayx(bank_account, razorpayx_config) {
-	return frappe.call({
-		method: "razorpayx_integration.razorpayx_integration.utils.bank_transaction.sync_transactions_for_reconcile",
-		args: { bank_account, razorpayx_config },
-		freeze: true,
-		freeze_message: __("Syncing Transactions. Please wait it may take a while..."),
-	});
+    return frappe.call({
+        method: "razorpayx_integration.razorpayx_integration.utils.bank_transaction.sync_transactions_for_reconcile",
+        args: { bank_account, razorpayx_config },
+        freeze: true,
+        freeze_message: __("Syncing Transactions. Please wait it may take a while..."),
+    });
 }
 
 async function toggle_sync_btn(frm) {
-	const btn = frm.custom_buttons[SYNC_BTN_LABEL];
+    const btn = frm.custom_buttons[SYNC_BTN_LABEL];
 
-	if (!btn) return;
+    if (!btn) return;
 
-	if (!frm.doc.bank_account) {
-		btn.hide();
-		return;
-	}
+    if (!frm.doc.bank_account) {
+        btn.hide();
+        return;
+    }
 
-	const { name } = await razorpayx.get_razorpayx_config(frm.doc.bank_account);
-	frm.__razorpayx_config = name;
+    const { name } = await razorpayx.get_razorpayx_config(frm.doc.bank_account);
+    frm.__razorpayx_config = name;
 
-	if (name) btn.show();
-	else btn.hide();
+    if (name) btn.show();
+    else btn.hide();
 }
